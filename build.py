94f8caeead66665ce79fbdd9fc152e9ec89c476e
Warren Kumari <warren@kumari.net>
2022-04-07 15:30:10 -0400

    Fix python env and whitespace

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """This reads a set of templates and parameters and builds JunOS configuration."""
 #
 # In order to make this easy to use, there are a number of
